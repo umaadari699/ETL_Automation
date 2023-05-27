61b217fdaa9036e9bebf6790eedfad9524e1b910
umaadari699 <132004964+umaadari699@users.noreply.github.com>
2023-05-27 11:27:43 +0530

    Update Sample_Program.py
    Added new logic

--- Sample_Program.py
+++ Sample_Program.py
@@ -19,19 +19,23 @@ rep = comp.report()  # Complete report generates
 print(rep)
 notInA = comp.df1_unq_rows  # Not present in DF1 frame
 notInB = comp.df2_unq_rows  # not Present in DF2 frame
+c= comp.all_mismatch()
 # Mismatched records values for matched IDs
-Mismatch = datacompy.Compare(df1, df2, join_columns='EMPLOYEE_ID')
-# Appended both DF rows and Remove duplicates so Unmatched records will be return as Output
-df3 = df1.append(df2)
-Non_Matched_rows = df3.drop_duplicates(keep=False)
-print(Non_Matched_rows.to_string())
-Mismatch_1 = Mismatch.all_mismatch()  # Mismatched records
-df4 = list(Mismatch_1)
-col=0
-for col in len(df4.columns):
-    if df4[col + 1] != df4[col + 2]:
-        print(df4[col + 1], df4[col + 2])
+# Mismatch = datacompy.Compare(df1, df2, join_columns='EMPLOYEE_ID')
+# # Appended both DF rows and Remove duplicates so Unmatched records will be return as Output
+# df3 = df1.append(df2)
+# Non_Matched_rows = df3.drop_duplicates(keep=False)
+# print(Non_Matched_rows.to_string())
+# Mismatch_1 = Mismatch.all_mismatch()  # Mismatched records
+
+df4=pd.merge(df1,df2,how='outer',indicator=True,on='employee_id')
+df5=pd.merge(df1,df2,how='outer',indicator=True,suffixes=(df1,df2),sort=True) # It Gives good results as expected
+print(df4)
+print(df5.to_string())
+
 with pd.ExcelWriter("results3.xlsx") as writer:
     notInA.to_excel(writer, sheet_name="NotinDF2", index=False)
     notInB.to_excel(writer, sheet_name="NotinDF1", index=False)
-    Mismatch_1.to_excel(writer, sheet_name="Mismatch Recs", index=False)
+    c.to_excel(writer, sheet_name="Mismatch Recs", index=False)
+    df5.to_excel(writer, sheet_name="Unmatched value Recs", index=False)
+# Return
